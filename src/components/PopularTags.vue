dc4982d163b00875d6f0f60dc4d3912362b1f05d
6ang5tar <cailansxq@gmail.com>
2023-02-13 01:00:31 +0900

    Create basic template, home page, popular tags

---
+++ src/components/PopularTags.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { api } from "../api";
+
+const tags = ref<String[]>([]);
+
+tags.value = await api.tags.getTags().then((res) => res.data.tags);
+</script>
+
+<template>
+  <div class="col-md-3">
+    <div class="sidebar">
+      <p>Popular Tags</p>
+
+      <div class="tag-list">
+        <a v-for="tag in tags" href="" class="tag-pill tag-default" >{{ tag }}</a>
+      </div>
+    </div>
+  </div>
+</template>
