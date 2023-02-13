d748703858fa9f6b75ef13f4edb438635a701113
6ang5tar <cailansxq@gmail.com>
2023-02-13 21:33:36 +0900

    Create ArticleList component

---
+++ src/components/ArticleList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-for="article in articles" class="article-preview">
+    <div class="article-meta">
+      <!-- TODO route -->
+      <a href="/profile/{{ article.author.username }}"
+        ><img :src="article.author.image" />
+      </a>
+      <div class="info">
+        <!-- TODO route -->
+        <a href="/profile/{{ article.author.username }}" class="author">
+          {{ article.author.username }}
+          <!-- TODO date format -->
+        </a>
+        <span class="date">{{ article.createdAt }}</span>
+      </div>
+      <button class="btn btn-outline-primary btn-sm pull-xs-right">
+        <i class="ion-heart"></i> {{ article.favoritesCount }}
+      </button>
+    </div>
+    <!-- TODO article route -->
+    <a href="/article/{{ article.slug }}" class="preview-link">
+      <h1>{{ article.title }}</h1>
+      <p>{{ article.description }}</p>
+      <span>Read more...</span>
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Article } from "../api/api";
+import { ref } from "vue";
+import { api } from "../api";
+
+const articles = ref<Article[]>([]);
+articles.value = (await api.articles.getArticles()).data.articles;
+</script>
